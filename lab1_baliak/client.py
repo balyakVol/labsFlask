1e69b8f835be8e212196892666c26ec75b5d2580
balyakVol <149668526+balyakVol@users.noreply.github.com>
2023-11-22 18:41:58 +0200

    Add files via upload

---
+++ lab1_baliak/client.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 25050
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+while input != "":
+    data = input("Enter something to send: ")
+
+    sock.sendall(data.encode())
+    data = sock.recv(1024)
+
+    print(f"Received {data!r}")
---
+++ lab1_baliak/server.py
@@ -0,0 +1,28 @@
+import socket
+from datetime import datetime
+import time
+
+HOST = "127.0.0.1"
+PORT = 25050
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.listen()
+conn, addr = sock.accept()
+with conn:
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        print("Timeout is on...")
+        time.sleep(5)
+        print("Timeout is off...")
+        
+        conn.sendall(data)
+        print(f"\t[Message] - {data.decode()}")
+        print(f"\t[Time] - {datetime.now()}")
+
+conn.close()
+sock.close()
